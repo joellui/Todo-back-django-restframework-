d43b30f2e071315a7c6bf91f59be8797f68345a4
Joel Louis <joellouis2000@gmail.com>
2023-02-02 22:21:39 +0530

    CRUD completed with user auth

--- todo/todo_api/urls.py
+++ todo/todo_api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import TodoListApiView
+from . import views
 
 urlpatterns = [
-    path('', TodoListApiView.as_view()),
+    path('', views.apiOverview, name='api-Overview'),
+    path('task-list/', views.taskList),
+    path('task-detail/<int:todo_id>', views.taskDetail),
+    path('task-update/<int:todo_id>', views.taskUpdate),
+    path('task-create', views.taskCreate),
+    path('task-delete/<int:todo_id>', views.taskDelete)
 ]
--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -1,36 +1,62 @@
-from django.shortcuts import render
-
-from rest_framework.views import APIView
-from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 from todo_api.serializers import TodoSerializer
 from  .models import Todo
 
-class TodoListApiView(APIView):
-    # Check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/task-list/',
+        'Detail View' : '/task-detail/<int:id>/',
+        'Create' : '/task-create/',
+        'Update' : '/task-update/<int:id>/',
+        'Delete' : '/task-delete/<int:id>/',
+    }
+    return Response(api_urls, status=status.HTTP_200_OK)
 
-    # List all
-    def get(self, request):
-        # all todo for given user
-        todos = Todo.objects.filter(user = request.user.id)
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def taskList(request):
+    todos = Todo.objects.filter(user = request.user.id)
+    serializer = TodoSerializer(todos,many= True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # Create
-    def post(self, request):
-        # create the todo
-        data = {
-            'task': request.data.get('task'),
-            'completed': request.data.get('completed'),
-            'user': request.user.id
-        }
-        serializer = TodoSerializer(data=data)
+# Get Task List
+@api_view(['GET'])
+def taskDetail(request, todo_id):
+    tasks = Todo.objects.get(id=todo_id)
+    serialize = TodoSerializer(tasks, many=False)
+    return Response(serialize.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+# Update Task
+@api_view(['POST'])
+def taskUpdate(request, todo_id):
+    task = Todo.objects.get(id=todo_id)
+    serializer = TodoSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Create Task
+@api_view(['POST'])
+def taskCreate(request):
+
+    data = {
+        'task': request.data.get('task'),
+        'completed': request.data.get('completed'),
+        'user': request.user.id
+    }
+    serializer = TodoSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Delete Task
+@api_view(['DELETE'])
+def taskDelete(request, todo_id):
+    task = Todo.objects.get(id = todo_id)
+    task.delete()
+    return Response(status=status.HTTP_508_LOOP_DETECTED)
+    
