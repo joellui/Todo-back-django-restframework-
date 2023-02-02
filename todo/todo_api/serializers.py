8f8d63ab988045e32852d50b282e8ebca4d58330
Joel Louis <joellouis2000@gmail.com>
2023-02-02 20:38:56 +0530

    Created GET and POST request

--- todo/todo_api/serializers.py
+++ todo/todo_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ["task", "completed", "timestamp", "updated", "user"]
+        fields = ["task", "completed", "timeStamp", "updated", "user"]
--- todo/todo_api/urls.py
+++ todo/todo_api/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path
 from .views import TodoListApiView
 
 urlpatterns = [
-    path('api', TodoListApiView.as_view())
+    path('', TodoListApiView.as_view()),
 ]
